534587b48e1ac2f0ca06470885820cb229b72cf3
kevinbrianndungu <kevinbrianndungu@gmail.com>
2024-02-25 11:34:13 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    if char not in [ord('e'), ord('q')]:
-        print("{}".format(chr(char)), end="")
-print()  # Print a newline at the end
